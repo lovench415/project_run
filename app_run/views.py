6f25a74610a5bef11575f224e09bf54d1e0186e0
lovench415 <lovench415>
2025-07-14 19:36:50 +0900

    none

--- app_run/views.py
+++ app_run/views.py
@@ -77,17 +77,15 @@ class StopRunAPIView(APIView):
 class AthleteInfoAPIView(APIView):
     def get(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
-        if not user.is_superuser:
-            athl, crt = AthleteInfo.objects.get_or_create(athlete=user)
-            serialize = AthleteInfoSerializer(athl)
-            return Response(serialize.data, status=status.HTTP_200_OK)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+        athl, crt = AthleteInfo.objects.get_or_create(athlete=user)
+        serialize = AthleteInfoSerializer(athl)
+        return Response(serialize.data, status=status.HTTP_200_OK)
 
     def put(self, request, user_id):
         user = get_object_or_404(User, id=user_id)
         serrializer = AthleteInfoSerializer(athlete=request.data)
         if serrializer.is_valid():
-            if not user.is_superuser and serrializer.data['weight'] < 900 and serrializer.data['weight'] > 0:
+            if serrializer.data['weight'] < 900 and serrializer.data['weight'] > 0:
                 AthleteInfo.objects.update_or_create(athlete=user, defaults={'goals': serrializer.data['goals'], 'weight': serrializer.data['weight']})
                 return Response(status=status.HTTP_201_CREATED)
         return Response(status=status.HTTP_400_BAD_REQUEST)
