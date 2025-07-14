89bacfd06e76cc5f09bdb4fd208daa593551d981
lovench415 <lovench415>
2025-07-14 18:46:46 +0900

    none

--- app_run/models.py
+++ app_run/models.py
@@ -15,4 +15,3 @@ class Run(models.Model):
 class AthleteInfo(models.Model):
     goals = models.TextField(default='')
     weight = models.PositiveSmallIntegerField(default=0)
-    user_id = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
