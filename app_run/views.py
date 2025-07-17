8c86436c0fc86ba3d4f9671c92a8c5351796318b
lovench415 <lovench415>
2025-07-17 12:22:38 +0900

    none

--- app_run/views.py
+++ app_run/views.py
@@ -116,14 +116,14 @@ def athlete_info(request, user_id):
             wgth = ''
         data_us = {'user_id': user_id, 'goals': serializer.data['goals'], 'weight': wgth}
         return JsonResponse(data_us)
-    else:
-        serrializer = AthleteInfoSerializer(data=request.data)
-        if serrializer.is_valid():
-            AthleteInfo.objects.update_or_create(athlete=user,
-                                                 defaults={'goals': serrializer.data['goals'],
-                                                           'weight': serrializer.data['weight']})
-            return Response(status=status.HTTP_201_CREATED)
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    # else:
+    #     serrializer = AthleteInfoSerializer(data=request.data)
+    #     if serrializer.is_valid():
+    #         AthleteInfo.objects.update_or_create(athlete=user,
+    #                                              defaults={'goals': serrializer.data['goals'],
+    #                                                        'weight': serrializer.data['weight']})
+    #         return Response(status=status.HTTP_201_CREATED)
+    #     return Response(status=status.HTTP_400_BAD_REQUEST)
 
 
 class ChallengeUserReadOnlyModelViewSet(viewsets.ReadOnlyModelViewSet):
